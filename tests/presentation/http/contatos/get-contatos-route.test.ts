9805fcf8cadbc10e3a29d26184c3a01779dad9bd
Alisson Vargas <contato@alissonsv.com>
2024-12-31 11:29:25 -0300

    test: add get contatos route e2e tests

---
+++ tests/presentation/http/contatos/get-contatos-route.test.ts
@@ -0,0 +1,52 @@
+import request from "supertest";
+
+import { app } from "#/main/app";
+import { prisma } from "#/lib/prisma";
+import { GetContatosUsecase } from "#/data/usecases/get-contatos-usecase";
+
+describe("Get Contatos route (e2e)", () => {
+  test("Should return 200 with a list of contacts sorted by name", async () => {
+    await prisma.contato.createMany({
+      data: [
+        {
+          nome: "John Doe",
+          telefone: "1111111111",
+        },
+        {
+          nome: "Jane Doe",
+          telefone: "2222222222",
+        },
+      ],
+    });
+
+    const response = await request(app).get("/contatos");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([
+      {
+        id: expect.any(Number),
+        nome: "Jane Doe",
+        telefone: "2222222222",
+      },
+      {
+        id: expect.any(Number),
+        nome: "John Doe",
+        telefone: "1111111111",
+      },
+    ]);
+  });
+
+  test("Should return 200 with an empty list if no contacts was created", async () => {
+    const response = await request(app).get("/contatos");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+
+  test("Should return 500 if any unexpected error occours", async () => {
+    jest
+      .spyOn(GetContatosUsecase.prototype, "execute")
+      .mockRejectedValueOnce(new Error());
+
+    await request(app).get("/contatos").expect(500);
+  });
+});
